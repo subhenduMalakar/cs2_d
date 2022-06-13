ba8fe26abe8c3ec576f93a1f5afb96207c567ec9
unknown <subhendumalakar19@gmail.com>
2022-06-13 19:59:15 +0530

    first commit

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def get_pre():
     return render_template('index.html',result=keys[values.index(ind)]) 
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0',port=80,debug=True)
